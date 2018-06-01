e3ab77ad9c2f46547cc80f26d62d381c312e7a38
Ady <adi@adi.com>
2018-06-01 16:48:40 +0300

    Added meetups pictures and php pages.

---
+++ meetups/header-menu.php
@@ -0,0 +1,24 @@
+<header id="header">
+    <div class="container">
+        <div id="logo" class="pull-left">
+            <a href="../index.html"><img src="../img/logo-header.png" alt="Scienceroot logo" title="Scienceroot" /></a>
+        </div>
+
+        <nav id="nav-menu-container">
+            <ul class="nav-menu">
+                <li class="menu-active"><a href="../index.html">Home</a></li>
+                <li><a href="../index.html#about">About Us</a></li>
+                <li><a href="../index.html#features">Features</a></li>
+                <li><a href="../index.html#team">Team</a></li>
+                <li><a href="https://medium.com/scienceroot" target="_blank">Blog</a></li>
+                <li><a href="../index.html#contact">Contact Us</a></li>
+                <li class="menu-has-children"><a href="#" class="disable download_resources">Resources</a>
+                <ul>
+                    <li><a href="../resources/onepager.pdf" target="_blank">One Pager</a></li>
+                    <li><a href="../resources/whitepaper.pdf" target="_blank">Whitepaper</a></li>
+                </ul>
+                </li>
+            </ul>
+        </nav>
+    </div>
+</header>
