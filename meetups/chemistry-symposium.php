c843c9fd229c67f483ca6124a7c2361960bf191a
Ady <adi@adi.com>
2018-06-22 23:45:37 +0300

    index.html: Added two new meetups. Added meetups cover pictures (TM and Frankfurt) Added chemistry-symposium.php and content-shift.php

---
+++ meetups/chemistry-symposium.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <title>Scienceroot | Blockchain International Show</title>
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+  <meta name="description" content="We are combining collaboration, funding and publishing with the help of blockchain to solve the requirements of every researcher, scientist and academic. With the help of Ethereum Smart Contracts and IPFS we manage to create the most efficient, intuitive and transparent ecosystem to date." />
+  <meta name="keywords" content="blockchain, scientific, ecosystem, academic, Research Blockchain, Academic Blockchain, Academic Ecosystem, Research Ecosystem, Funding Blockchain, Funding Research, Funding Ecosystem, Scholarly Blockchain,
+  Publishing Blockchain, Scholar Blockchain, Scholar Ecosystem">
+  <meta property="og:title" content="Scienceroot | First Blockchain-Based Scientific Ecosystem">
+  <meta property="og:description" content="We are combining collaboration, funding and publishing with the help of blockchain to solve the requirements of every researcher, scientist and academic. With the help of Ethereum Smart Contracts and IPFS we manage to create the most efficient, intuitive and transparent ecosystem to date.">
+  <meta property="og:url" content="https://scienceroot.com/index.html">
+  <meta property="og:image" content="https://scienceroot.com/img/scienceroot-share-logo.png">
+  <meta property="og:image:width" content="1200"/>
+  <meta property="og:image:height" content="1200"/>
+  <meta name="twitter:title" content="Scienceroot | First Blockchain-Based Scientific Ecosystem">
+  <meta name="twitter:description" content="We are combining collaboration, funding and publishing with the help of blockchain to solve the requirements of every researcher, scientist and academic. With the help of Ethereum Smart Contracts and IPFS we manage to create the most efficient, intuitive and transparent ecosystem to date.">
+  <meta name="twitter:card" content="summary_large_image">
+  <link href="../img/favicon.png" rel="icon">
+  <link href="../img/favicon.png" rel="apple-touch-icon">
+  <link href="https://fonts.googleapis.com/css?family=Montserrat:300,400,500,700|Open+Sans:300,300i,400,400i,700,700i" rel="stylesheet">
+  <link href="../lib/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="../lib/animate/animate.min.css" rel="stylesheet">
+  <link href="../lib/font-awesome/css/font-awesome.min.css" rel="stylesheet">
+  <link href="../lib/ionicons/css/ionicons.min.css" rel="stylesheet">
+  <link href="../lib/magnific-popup/magnific-popup.css" rel="stylesheet">
+  <link href="../css/meetup.css" rel="stylesheet">
+  <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-WFKNG47');
+  </script>
+</head>
+
+<body>
+  <?php require_once('header-menu.php'); ?>
+  <main id="main">
+    <section id="about">
+      <div class="container">
+        <div class="section-header">
+          <h3 class="section-title">Chemistry Symposium</h3>
+          <span class="section-divider"></span>
+          <p class="section-description">
+            28-29.06.2018<br />
+            Timisoara <br />
+            <a href="http://acad-icht.tm.edu.ro/cercetareRO.html" target="_blank">
+            http://acad-icht.tm.edu.ro/cercetareRO.html</a>
+          </p>
+        </div>
+      </div>
+    </section>
+  </main>
+
+  <script async src="https://www.googletagmanager.com/gtag/js?id=UA-109748162-1"></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', 'UA-109748162-1');
+  </script>
+  <script src="../lib/jquery/jquery.min.js"></script>
+  <script src="../lib/jquery/jquery-migrate.min.js"></script>
+  <script src="../lib/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="../lib/easing/easing.min.js"></script>
+  <script src="../lib/wow/wow.min.js"></script>
+  <script src="../lib/superfish/hoverIntent.js"></script>
+  <script src="../lib/superfish/superfish.min.js"></script>
+  <script src="../lib/magnific-popup/magnific-popup.min.js"></script>
+  <script src="../js/main.js"></script>
+</body>
+</html>
